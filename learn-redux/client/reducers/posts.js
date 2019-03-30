f7106f748376f292efcfb7d1e4a8296850b10f56
Robert Chung <chungrob@gmail.com>
2019-03-30 10:25:54 -0400

    First Reducer

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,9 +4,23 @@
 
 
 function posts(state = [], action) {
-    console.log('the post will change');
-    console.log(state, action);
-    return state;
+    // console.log('the post will change');
+    // console.log(state, action);
+    switch(action.type) {
+        case 'INCREMENT_LIKES':
+            console.log('incrementing likes');
+            const i = action.index;
+            return [
+                ...state.slice(0, i), // before the one we are updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1),
+            ]
+
+        // return the updated state
+        default:
+            return state;
+    } 
+    // return state;
 }
 
 export default posts;
