02b161fdf2873deed6d6ee9f0c7d3fad782eac15
Robert Chung <chungrob@gmail.com>
2019-03-30 12:30:23 -0400

    Post and Delete Comments

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -9,7 +9,7 @@ const Comments = React.createClass({
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className="remove-comment"> &times; </button>
+                    <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}> &times; </button>
                 </p>
             </div>
         )
@@ -23,6 +23,7 @@ const Comments = React.createClass({
         const comment = this.refs.comment.value;
         // console.log(postId, user, comment, 'yeah!!!');
         this.props.addComment(postId, user, comment);
+        this.refs.commentForm.reset();
 
     },
     render () {
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,37 @@
+function postComments(state = [], action) {
+    switch(action.type){
+        case 'ADD_COMMENT':
+        // return the new state with the new comment
+        return [...state, {
+            user:action.author,
+            text: action.comment
+        }];
+        case 'REMOVE_COMMENT':
+            // console.log('===removing a comment !!===');
+            // we need to retun the new state with out the deleted comment 
+            return [
+                //from teh start to the one we want ot delete
+                ...state.slice(0, action.i),
+                //after the deleted one, to the end
+                ...state.slice(action.i + 1)
+            ]
+            // return state;
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments(state = [], action) {
-    console.log('comment state', state, action);
+    // console.log('comment state', state, action);
+    if (typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            //overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
