086c0bdc180b3232f91d9417046b905112884d91
Robert Chung <robertchung@Roberts-MacBook-Pro.local>
2019-06-24 11:28:03 -0400

    Base Component

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Main = React.createClass({
-    render(){
+    render() {
         return (
             <div>
-                <h1>
-                <Link to="/">Reduxstagram</Link>
-                </h1>
-                {React.cloneElement(this.props.children, this.props)}
+                <h1>Robuxtstragram</h1>
             </div>
         )
     }
 });
-
 
 export default Main;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,64 +1,13 @@
 // let's go!
-// import React from '../../../../../Library/Caches/typescript/2.9/node_modules/@types/react';
 import React from 'react';
-
-
-// import { render } from '../../../../../Library/Caches/typescript/2.9/node_modules/@types/react-dom';
 
 import { render } from 'react-dom';
 
-
-// Import css
-
+//Import CSS
 import css from './styles/style.styl';
 
-// Impport Components
-import App from './components/App';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+// Import Components
+import Main from './components/Main';
 
+render(<Main />, document.getElementById('root'));
 
-//import react router deps
-// import { Router, Route, IndexRoute, browserHistory } from '../../../../../Library/Caches/typescript/2.9/node_modules/@types/react-router';
-
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import store, { history } from './store';
-
-
-// === Sentry stuff ======
-import Raven from 'raven-js';
-import { sentry_url, logException } from './data/config';
-
-Raven.config(sentry_url, {
-    tags:{
-        git_commit: 'jkljlkjkj;',
-        userLevel: 'editor'
-    }
-}).install();
-
-logException(new Error('download failed!'), {
-    email: 'wesbos@gmail.com'
-});   
-
-// Raven.captureMessage('Something bad happened!');
-// Raven.showReportDialog();
-
-//===== ^ Sentry====
-
-
-const router = (
-    <Provider store={store}>
-        <Router history={history}>
-            <Route path = "/" component={App}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="view/:postId" component={Single}></Route>
-            </Route>
-        </Router>
-    </Provider>
-
-)
- 
-render(router, document.getElementById('root'));
-
- 
