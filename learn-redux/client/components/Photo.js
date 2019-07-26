7b08f9c429f6023b2d9ad5da89651ef3355ec3ac
Robert Chung <chungrob@gmail.com>
2019-07-26 00:31:13 -0400

    First Reducer for posts - see Comments

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group'
 
@@ -19,7 +19,7 @@ const Photo = React.createClass({
                 <figcaption>
                     <p>{post.caption}</p>
                     <div className="control-buttons">
-                        <button className="likes">&hearts;{post.likes}</button>
+                        <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts;{post.likes}</button>
                         <Link to={`/view/${post.code}`}>
                             <span className="comment-count">
                                 <span className="speech-bubble">
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -7,9 +7,29 @@
 // Then update the new state of the store
 
 function posts(state = [], action) {
-    console.log('THE POST WILL CHANGE');
-    console.log('POSTS JS STATE: ', state, 'POSTS JS ACTION: ', action);
-    return state;
+    // console.log('THE POST WILL CHANGE');
+    // console.log('POSTS JS STATE: ', state, 'POSTS JS ACTION: ', action);
+    // Reducer when we click on like, increment likes is what we need to act on
+    // Switch statement is what allows us to act
+
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            console.log('INCREMENTING LIKES!!');
+            const i = action.index;
+            return [
+                ...state.slice(0, i), //before the one we are updating
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1), //after the one we are updating
+            ]
+            // return the updated state
+            default: 
+            return state;
+    }
 }
 
 export default posts;
+
+// Video 12:
+// Redux - don't mutate state, use pure functions
+// Takes a copy of the old state, returns a copy of the new state
+// Old state shouldn't be mutated
